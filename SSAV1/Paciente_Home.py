5031bc46e190e857dbb55635e2ccf5e5a95d9493
josesaldanapeople <jose.saldana@people-media.com.mx>
2025-01-03 17:09:15 -0600

    cambio de decoradores y metodos

---
+++ SSAV1/Paciente_Home.py
@@ -0,0 +1,27 @@
+import psycopg2
+from flask import Flask, jsonify
+from db import Acceso
+
+app = Flask(__name__)
+
+@app.route('/Paciente/Home/EsquemaBasico/<curp>')
+def EsquemaBasicoPaciente(curp):
+    condiciones = {
+        'curp': ('=', curp)
+    }
+    data = Acceso("vwEsquemaBasicoPaciente").EjecutaVista(condiciones)
+    return jsonify(data)
+
+
+
+
+
+
+#@app.route('/esquemabasico/test')
+#def esquemafn():
+#    params = ["10", "5", "20"]
+#    data = Acceso("funcion_prueba",params).EjecutaFuncion()
+#    return jsonify(data)
+#
+#if __name__ == '__main__':
+#    app.run(debug=True)  
