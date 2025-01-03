b9f3a56177ed165fde33f1a03b16aa1f80164570
josesaldanapeople <jose.saldana@people-media.com.mx>
2025-01-03 17:18:12 -0600

    cambio en ruteo de webmethods

--- SSAV1/Paciente_Home.py
+++ SSAV1/Paciente_Home.py
@@ -4,13 +4,20 @@ from db import Acceso
 
 app = Flask(__name__)
 
-@app.route('/Paciente/Home/EsquemaBasico/<curp>')
+@app.route('/Paciente/Home/<curp>')
 def EsquemaBasicoPaciente(curp):
     condiciones = {
         'curp': ('=', curp)
     }
-    data = Acceso("vwEsquemaBasicoPaciente").EjecutaVista(condiciones)
-    return jsonify(data)
+    vwEsquemaBasicoPaciente_ = Acceso("vwEsquemaBasicoPaciente").EjecutaVista(condiciones)
+    vwSiguientesVacPaciente_ = Acceso("vwSiguientesVacPaciente").EjecutaVista(condiciones)
+    vwOtrasVacPaciente_ = Acceso("vwOtrasVacPaciente").EjecutaVista(condiciones)
+    Respuesta = {
+        'vwEsquemaBasicoPaciente':vwEsquemaBasicoPaciente_,
+        'vwSiguientesVacPaciente':vwSiguientesVacPaciente_,
+        'vwOtrasVacPaciente':vwOtrasVacPaciente_
+    }
+    return jsonify(Respuesta)
 
 
 
@@ -23,5 +30,5 @@ def EsquemaBasicoPaciente(curp):
 #    data = Acceso("funcion_prueba",params).EjecutaFuncion()
 #    return jsonify(data)
 #
-#if __name__ == '__main__':
-#    app.run(debug=True)  
+if __name__ == '__main__':
+    app.run(debug=True)  
